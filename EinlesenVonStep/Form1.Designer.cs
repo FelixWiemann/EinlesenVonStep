62f17913a8cc7d15bd1d347ceaacaa3ea3233656
Felix Wiemann <felix.wiemann@web.de>
2016-06-03 16:59:06 +0200

    Little testapplication for showing what vectordraw in combination with vector is capable to do. This application is capable of drawing points stored in a .stp file so the user may guess what is stored in the file. The primitve of the primitve CAD programs...

---
+++ EinlesenVonStep/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace EinlesenVonStep
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.vectorDraw1 = new nepumuk.VectorDraw();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.vectorDraw1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 197);
+            this.textBox1.TabIndex = 0;
+            // 
+            // vectorDraw1
+            // 
+            this.vectorDraw1.AxisColor = System.Drawing.Color.Black;
+            this.vectorDraw1.Location = new System.Drawing.Point(278, 12);
+            this.vectorDraw1.Name = "vectorDraw1";
+            this.vectorDraw1.Size = new System.Drawing.Size(1002, 846);
+            this.vectorDraw1.TabIndex = 1;
+            this.vectorDraw1.TabStop = false;
+            this.vectorDraw1.VectorColor = System.Drawing.Color.Black;
+            this.vectorDraw1.ZoomFactor = 1D;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 220);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 246);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(93, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(93, 272);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1292, 870);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.vectorDraw1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.vectorDraw1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private nepumuk.VectorDraw vectorDraw1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ EinlesenVonStep/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using nepumuk;
+using System.Globalization;
+
+namespace EinlesenVonStep
+{
+    public partial class Form1 : Form
+    {
+
+        String[] stpDat;
+        String din;
+        String FileDescritpion;
+        String FileName;
+        String FileShema;
+        String FileCreationDate;
+       
+     //   int[] Data;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stpDat = textBox1.Text.Split('\n');
+           // textBox1.Text = "";
+            din = stpDat[0].Substring(0,stpDat[0].Length-3);
+            foreach (string a in stpDat)
+            {
+                interpreteString(a);
+                //textBox1.Text += a + " new line \n";
+            }
+            writeLabels();
+        }
+
+        private void interpreteString(string s)
+        {
+            int currentTask;
+            string instruktion;
+            string rest;
+
+            if (s.Contains("FILE_DESCRIPTION"))
+            {
+                FileDescritpion = s.Substring(s.IndexOf("(('") + 3, s.IndexOf("'),") - s.IndexOf("(('")-3);
+            }
+            if (s.Contains("FILE_NAME"))
+            {
+                FileName = s.Substring(s.IndexOf("('")+2, s.IndexOf("','") - s.IndexOf("('")-2);
+                FileCreationDate = s.Substring(s.IndexOf("','") + 3, s.IndexOf("',('") - s.IndexOf("','")-3);
+
+            }
+            if (s.Contains("FILE_SCHEMA"))
+            {   
+                FileShema = s.Substring(s.IndexOf("(('") + 3, s.IndexOf("'))") - s.IndexOf("(('")-3);
+            }
+            if (s.Contains("#"))
+            {
+               currentTask= Convert.ToInt32( s.Substring(1, s.IndexOf('=')-1));
+               instruktion = s.Substring(s.IndexOf('=') + 1, s.IndexOf('(') - 1 - s.IndexOf('='));
+               rest = s.Substring(s.IndexOf('(') + 1, s.Length - 1 - s.IndexOf('('));
+
+               switch (instruktion)
+               {
+                   case "CARTESIAN_POINT":
+                       rest = rest.Substring(rest.IndexOf(',')+1);
+                       double x, y, z;
+                       x = Convert.ToDouble(rest.Substring(rest.IndexOf("(") + 1, rest.IndexOf(',') - rest.IndexOf("(")-1), CultureInfo.InvariantCulture.NumberFormat);
+                       rest = rest.Substring(rest.IndexOf(',') + 1);
+                       y = Convert.ToDouble(rest.Substring(0, rest.IndexOf(',')), CultureInfo.InvariantCulture.NumberFormat);
+                       rest = rest.Substring(rest.IndexOf(',') + 1);
+                       z = Convert.ToDouble(rest.Substring(0, rest.IndexOf(')')), CultureInfo.InvariantCulture.NumberFormat);
+                       vectorDraw1.DrawPoint(new Vector(x,y,z));
+                      
+                       break;
+
+                   default:
+
+
+                       break;
+               }
+
+
+
+
+            }
+
+            
+
+
+            
+        }
+
+        private void writeLabels()
+        {
+            label1.Text = ("Name: " + FileName);
+            label2.Text = ("Date of Creation: " + FileCreationDate);
+            label3.Text = ("Descritpion: " + FileDescritpion);
+            label4.Text = ("Shema: " + FileShema);
+            label5.Text = ("" + din);
+
+
+
+        }
+
+       
+
+        
+
+
+    }
+}
